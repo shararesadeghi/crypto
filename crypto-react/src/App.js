bcf56485aa5519b03392dd1de25bfdd5585f9e90
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-08-24 16:39:49 +0430

    Landing& Loader created

---
+++ crypto-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//components
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <div className="App">
+     <Landing/>
+    </div>
+  );
+}
+
+export default App;
---
+++ crypto-react/src/components/Landing.js
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react';
+
+//API
+import { getCoin } from '../services/api';
+//components
+import Loader from './Loader';
+
+
+const Landing = () => {
+
+    const [coins,setCoins] = useState([]);
+
+    useEffect(()=>{
+        const fetchAPI = async () =>{
+            const data = await getCoin();
+            setCoins(data);
+        }
+        fetchAPI();
+    },[]);
+    return (
+        <>
+         <input type = "text" placeholder = "Search"/>  
+         {
+             coins.length ?
+             <div>
+             {coins.map(coin => <p key = {coin.id}>{coin.name}</p>)}
+        </div> 
+        : <Loader/>
+         }
+         
+        </>
+    );
+};
+
+export default Landing;
